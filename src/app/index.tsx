3ec8b27dcecdc4e82ac87578510c5042c087116f
Ievgen Vorontsov <evgeniyvorontsov1@gmail.com>
2024-01-04 21:54:04 +0200

    load fonts in the Rootlayout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,34 +8,12 @@ import {
   ActivityIndicator,
 } from "react-native";
 import DayListItem from "../../src/components/core/DayListItem";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import {
-  AmaticSC_400Regular,
-  AmaticSC_700Bold,
-} from "@expo-google-fonts/amatic-sc";
 
-SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function HomeScreen() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold,
-  });
 
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -58,11 +36,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-
   column: {
     gap: 10,
   },
-
   content: {
     gap: 10,
     padding: 10,
