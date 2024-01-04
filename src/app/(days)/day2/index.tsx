eeef77370c2f305e22ec662c4da74fc9a1e2c826
Ievgen Vorontsov <evgeniyvorontsov1@gmail.com>
2024-01-04 21:31:43 +0200

    set up  expo router and the main screens

---
+++ src/app/(days)/day2/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DayDetailsScreen = () => {
+  return (
+    <View>
+      <Text>DayDetailsScreen</Text>
+    </View>
+  );
+}
+
+export default DayDetailsScreen;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,34 +7,35 @@ import {
   SafeAreaView,
   ActivityIndicator,
 } from "react-native";
-import DayListItem from "./src/components/core/DayListItem";
+import DayListItem from "../../src/components/core/DayListItem";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from 'react';
-import {AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
-
+import { useEffect } from "react";
+import {
+  AmaticSC_400Regular,
+  AmaticSC_700Bold,
+} from "@expo-google-fonts/amatic-sc";
 
 SplashScreen.preventAutoHideAsync();
 
-const days = [...Array(24)].map((val, index) => index +1);
+const days = [...Array(24)].map((val, index) => index + 1);
 
-export default function App() {
- const [fontsLoaded, fontError] = useFonts({
-   Inter: Inter_900Black,
-   Amatic: AmaticSC_400Regular,
-   AmaticBold: AmaticSC_700Bold,
- });
-  
+export default function HomeScreen() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold,
+  });
+
   useEffect(() => {
-     if (fontsLoaded || fontError) {
-       SplashScreen.hideAsync();
-     }
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
   }, [fontsLoaded, fontError]);
-  
-  if (!fontsLoaded && !fontError) { 
+
+  if (!fontsLoaded && !fontError) {
     return null;
   }
- 
 
   return (
     <SafeAreaView style={styles.container}>
