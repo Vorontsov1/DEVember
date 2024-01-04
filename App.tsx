0be549c5c6f89614e494e849d1d7319872795f19
Ievgen Vorontsov <evgeniyvorontsov1@gmail.com>
2024-01-04 13:33:06 +0200

    FlatrList

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+const days = [...Array(24)].map((val, index) => index +1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FlatList
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <View style={styles.box}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+
+  column: {
+    gap: 10,
+  },
+
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+
+  box: {
+    backgroundColor: "#f4d2b7",
+    flex: 1, 
+    aspectRatio: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#b65a41",
+    borderRadius: 20,
+  },
+  text: {
+    color: "#b65a41",
+    fontSize: 70,
   },
 });
